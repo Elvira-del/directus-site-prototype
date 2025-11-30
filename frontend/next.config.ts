d37a2dbf28f062e2414038d75f0b21a755d1c582
Elvira Krutova <ninelostr@gmail.com>
2025-11-30 21:02:02 +0000

    feat: add next.js frontend

---
+++ frontend/next.config.ts
@@ -0,0 +1,17 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "8055",
+        pathname: "/assets/**",
+      },
+    ],
+    dangerouslyAllowLocalIP: true,
+  },
+};
+
+export default nextConfig;
