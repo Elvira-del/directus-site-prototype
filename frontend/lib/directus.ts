6481a291582deb9672b02dc8ca2659a348febc2c
Elvira Krutova <ninelostr@gmail.com>
2025-12-16 03:10:18 +0000

    feat: add directus extensions and uploads

--- frontend/lib/directus.ts
+++ frontend/lib/directus.ts
@@ -1,7 +1,6 @@
 import { createDirectus, rest, authentication } from "@directus/sdk";
-import { Schema } from "@/types/api.types";
 
-const client = createDirectus<Schema>(process.env.NEXT_PUBLIC_BACKEND_URL as string)
+const client = createDirectus(process.env.NEXT_PUBLIC_BACKEND_URL as string)
   .with(authentication("json"))
   .with(
     rest({
